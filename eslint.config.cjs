be3f95d370b789921d3d8232fcc417df25c7288c
GabrielFuentes17 <2026010123@cvirtualuees.edu.sv>
2026-05-06 09:42:23 -0600

    chore: configurar eslint y script de lint

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'src/output.css'],
+  },
+  js.configs.recommended,
+  {
+    files: ['src/main.js', 'src/preload.js', 'src/db.js', 'src/auth/**/*.js', 'src/login/auth-db.js', 'src/src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'no-redeclare': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['src/tabs.js', 'src/login/login.js', 'src/login/home.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'script',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
